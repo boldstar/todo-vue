069cc4e0cfa3145878f959dd33a53c7cc101d168
Thomas <thomasweems3@gmail.com>
2018-12-09 15:29:52 -0600

    merge deploy branch

--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ import { abilityPlugin, ability as appAbility } from './utils/ability'
 export const ability = appAbility
 
 Vue.use(Vuex)
-axios.defaults.baseURL = 'https://todolaraveltest.club/api'
+axios.defaults.baseURL = 'https://backend.todovuetest.net/api'
 
 export default new Vuex.Store({
   plugins: [
